f9fe4630b3baa4320385a46c7154ff2513868b64
Wil Hunt <huntwj@gmail.com>
2015-12-02 17:38:01 -0800

    Add some "common" settings that most people will want.

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
 ; Put your module require statments here.
 /require tf-wotmud/main.tf
 
-; And you can put your own private stuff here.
-/def rh = \
-    ride horse
+/def key_esc_up = \
+    /dokey PAGEBACK
+/def key_esc_down = \
+    /dokey PAGE
+
+;; Customize system variables
+
+/test defCompile := "on"
+/test histsize   := 100000
+/test maildelay  := 0
+/test oldslash   := "off"
+/test pendantic  := "on"
+/test secho      := "on"
+/test warn5keys  := "off"
 
