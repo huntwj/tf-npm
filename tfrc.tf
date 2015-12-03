c589944eb9c03790a2354c5256ba5193fedfdee5
Wil Hunt <huntwj@gmail.com>
2015-12-02 16:25:13 -0800

    Add placeholder directory for data and custom variables.

--- tfrc.tf
+++ tfrc.tf
@@ -1,7 +1,8 @@
 ; Sample .tfrc file
 ;
 /eval /set TF_NPM_ROOT=%{HOME}/tf-npm
-/eval /set TFPATH=%{TFLIBDIR} %{TF_NPM_ROOT} %{TF_NPM_ROOT}/node_modules
+;/eval /set TFPATH=%{TFLIBDIR} %{TF_NPM_ROOT} %{TF_NPM_ROOT}/node_modules
+/eval /set TFPATH=%{TFLIBDIR} %{TF_NPM_ROOT} %{HOME}/tf-dev
 
 /load main.tf
 
