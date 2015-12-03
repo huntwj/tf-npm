c06462cad7bb585ba212745bb6ba190df32b5d6b
Wil Hunt <huntwj@gmail.com>
2015-12-02 21:36:52 -0800

    Fix .tfrc to make everything happy moving forward
    (and allow easy overriding for development)

--- tfrc.tf
+++ tfrc.tf
@@ -1,8 +1,8 @@
 ; Sample .tfrc file
 ;
 /eval /set TF_NPM_ROOT=%{HOME}/tf-npm
-;/eval /set TFPATH=%{TFLIBDIR} %{TF_NPM_ROOT} %{TF_NPM_ROOT}/node_modules
-/eval /set TFPATH=%{TFLIBDIR} %{TF_NPM_ROOT} %{HOME}/tf-dev
+/eval /set TF_NPM_MODULES_ROOT=%{HOME}/tf-npm/node_modules
+/eval /set TFPATH=%{TFLIBDIR} %{TF_NPM_ROOT} %{TF_NPM_MODULES_ROOT}
 
 /load main.tf
 
